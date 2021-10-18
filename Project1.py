2215df8eca8127a71ee34f374e44cac91258b8a4
Brett Kaliner <brett.kaliner149@gmail.com>
2021-10-18 10:40:00 -0400

    Sentiment Analyzer and Scrape

--- Project1.py
+++ Project1.py
@@ -1,3 +1,4 @@
+#Import packages
 import pandas as pd
 import requests
 import nltk
@@ -5,11 +6,12 @@ nltk.download('vader_lexicon')
 nltk.download('wordnet')
 from bs4 import BeautifulSoup
 
-
+#Create empty dataframe for companies
 randomCompanies = pd.DataFrame(columns=['Title', 'Purpose'])
 companyList = []
 linesList = []
 
+#Scrape website for name and purpose of company
 for x in range(50):
     url = "http://3.85.131.173:8000/random_company"
     page = requests.get(url)
@@ -22,12 +24,13 @@ for x in range(50):
     for li in soup.find_all('li'):
         linesList.append(li.get_text())
 
+#Fill dataframe with scrape
 matches = [s for s in linesList if "Purpose" in s]
 randomCompanies['Purpose'] = matches
 randomCompanies['Title'] = companyList
 
+#Get rid of word "purpose"
 randomCompanies['Purpose'] = randomCompanies['Purpose'].str.replace('Purpose: ', '')
 
+#Write company list to CSV
 randomCompanies.to_csv('CompanyPull.csv')
-
-print("done")
--- Sentiment_Analyzer.py
+++ Sentiment_Analyzer.py
@@ -1,3 +1,4 @@
+#Import Packages
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
@@ -6,39 +7,52 @@ nltk.download('wordnet')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import typing
 
+#Read in data from group members
 mike_data = pd.read_csv('fake_company_pull.csv')
 brett_data = pd.read_csv('CompanyPull.csv')
 steve_data = pd.read_csv('comps.csv')
 
+#Get rid of word "Purpose"
 mike_data['Purpose'] = mike_data['Purpose'].str.replace('Purpose: ', '')
 steve_data['Purpose'] = steve_data['Purpose'].str.replace('Purpose: ', '')
+
+#Gwt rid of unnecessary column
 brett_data = brett_data.iloc[: , 1:]
 
-
+#Put dataframes together
 allCompanies = pd.DataFrame(columns=['Title', 'Purpose'])
 allCompanies = allCompanies.append(mike_data)
 allCompanies = allCompanies.append(brett_data)
 allCompanies = allCompanies.append(steve_data)
 
+#Define stopwords
 stopcorpus: typing.List = stopwords.words('english')
 
+#Define function to remove stop words
 def remove_words(em:str,list_of_words_to_remove: typing.List):
     return [item for item in em if item not in list_of_words_to_remove]
+
+#Define function to make list into string
 def collapse_list_to_string(string_list):
     return ' '.join(string_list)
 
+#Run functions
 allCompanies['Purpose'] = allCompanies['Purpose'].astype(str).apply(lambda x: remove_words(x.split(),stopcorpus))
 allCompanies['Purpose'] = allCompanies['Purpose'].apply(collapse_list_to_string)
 
+#Set up tokenizer and lemmatizer
 tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
+
+#Define function to use lemmatizer and tokenizer
 def root_word(text):
     return [lemmatizer.lemmatize(w) for w in tokenizer.tokenize(text)]
 
+#Apply root word function
 allCompanies['Purpose'] = allCompanies['Purpose'].astype(str).apply(root_word)
 allCompanies['Purpose'] = allCompanies['Purpose'].apply(collapse_list_to_string)
 
-
+#Sentiment Analysis
 sentiment = []
 analyzer = SentimentIntensityAnalyzer()
 for purpose in allCompanies.Purpose:
@@ -46,13 +60,9 @@ for purpose in allCompanies.Purpose:
     sentiment.append(vs["compound"])
 allCompanies['Sentiment'] = sentiment
 
-
+#Sort the companies by sentiment score
 allCompanies.sort_values(by=['Sentiment'], inplace=True)
 worst_sentiment = allCompanies.head(5)
 best_sentiment = allCompanies.tail(5)
 print(best_sentiment)
 print(worst_sentiment)
-
-
-
-print("done")
